0352c972a8c34ca95be8d3c0b74febd2d5993a08
RomainMefret <romain.mefret@healthygaming.fr>
2021-04-18 01:30:47 +0200

    Partie 1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import "./index.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="bg-gray-200">Test</header>
-    </div>
+    <Router>
+      <Route path="/" exact render={() => <Home />}></Route>
+      <div className="App">
+        <header className="bg-gray-200">Test</header>
+      </div>
+    </Router>
   );
 }
 
